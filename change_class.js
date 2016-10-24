b6203420c138782726117312e60f03241b2d62f5
江哲 <jiang.zhe@xiaoyi.com>
2016-10-24 17:05:13 +0800

    update for 2016_CourseList

--- change_class.js
+++ change_class.js
@@ -11,16 +11,23 @@
  */
 
 $(function(){
-	var a = $('tr');
-	for(var i = 1; i < a.length; i++){
-		var t = a[i], s = t.innerText.split('\t'), j = s.length - 2;
-		if(t.innerText.indexOf('\t--\t') > 0){
-			$($(t).find('td')[1]).find('a').last()[0].click()
-			return;
-		}
-		if(s[0] == '返回课程列表' || j > 0 && s[j].match(/\d/) + '' == '0'){
-			$(t).find('a')[0].click();
-			return;
-		}
+    var a = $('tr');
+    for(var i = 1; i < a.length; i++){
+	var t = a[i], s = t.innerText.split('\t'), j = s.length - 2;
+	if(t.innerText.indexOf('《ERP与财务软件操作》　第十三讲') < 0 && t.innerText.indexOf('\t--\t') > 0){
+	    $($(t).find('td')[1]).find('a').last()[0].click();
+	    return;
 	}
+	if(s[0] == '返回课程列表'){
+	    $(t).find('a')[0].click();
+	    return;
+	}
+	if(!s[j] || s[0].indexOf('ERP与财务软件操作') > 0)
+	    continue;
+	var arr = s[j].match(/(\d+).(\d+)/);
+	if(j > 0 && arr && arr[1] != arr[2]){
+	    $(t).find('a')[0].click();
+	    return;
+	}
+    }
 });
--- set_time.js
+++ set_time.js
@@ -11,23 +11,23 @@
  */
 
 $(function(){
-	if($('audio').lenght>0){
-		$('audio')[0].playbackRate = 10000;
-		setInterval(function(){
-			var time = $('.jp-duration').text().split(':');
-			time = time[0] * 60 + parseInt(time[1]) - 1;
-			$('audio')[0].currentTime = time;
-		}, 5000);
-	}else{// 需要控制台手动运行
-		setInterval(function(){
-		var s=$('#WebJPlayer_Voide').data('jPlayer').status,t=s.currentTime;
-		if(t+20>s.duration){
-			return;
-		}else{
-			t+=1200;
-		}
-		if(s.duration<t)t=s.duration-20;$('#WebJPlayer_Voide').jPlayer('play', t);
-		console.log(s.duration+' '+t);
-		}, 5000);
-	}
+    if($('audio').length>0){
+	$('audio')[0].playbackRate = 10000;
+	setInterval(function(){
+	    var time = $('.jp-duration').text().split(':');
+	    time = time[0] * 60 + parseInt(time[1]) - 1;
+	    $('audio')[0].currentTime = time;
+	}, 5000);
+    }else{// 需要控制台手动运行
+	setInterval(function(){
+	    var s=$('#WebJPlayer_Voide').data('jPlayer').status,t=s.currentTime;
+	    if(t+20>s.duration){
+		return;
+	    }else{
+		t+=1200;
+	    }
+	    if(s.duration<t)t=s.duration-20;$('#WebJPlayer_Voide').jPlayer('play', t);
+	    console.log(s.duration+' '+t);
+	}, 5000);
+    }
 });
